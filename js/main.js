bdfd4ed908fe122dd1b833b8af7067a1404cf14c
favadu <duongpfiev51@gmail.com>
2016-02-19 17:31:24 +0100

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,82 @@
+/*!
+ * Start Bootstrap - Creative Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    })
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Fit Text Plugin for Main Header
+    $("h1").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    // Initialize WOW.js Scrolling Animations
+    new WOW().init();
+
+
+    $('span.btn-details').bind('click', function(event) {
+        var detailPart = $('#' + $(this).attr('data-toggle'));
+        var dataAction =  $(this).attr('data-action');
+        if (dataAction == 'show') {
+            detailPart.addClass("in");
+            $(this).text('Moin détail');
+            $(this).attr('data-action', 'hide');
+        } else {
+            detailPart.removeClass("in");
+            $(this).text('Plus détail');
+            $(this).attr('data-action', 'show');
+
+        }
+        //event.preventDefault();
+    });
+
+
+})(jQuery); // End of use strict
